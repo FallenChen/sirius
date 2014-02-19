49818c35b0d64fb34e4ef20403d5555f5e0cec79
aha <aha@scireum.de>
2014-02-19 14:28:20 +0100

    Integrated automatic SCSS to CSS compiler

--- web/src/sirius/web/dispatch/AssetsDispatcher.java
+++ web/src/sirius/web/dispatch/AssetsDispatcher.java
@@ -10,15 +10,21 @@ package sirius.web.dispatch;
 
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import org.serversass.Generator;
+import sirius.kernel.Sirius;
 import sirius.kernel.commons.PriorityCollector;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.commons.Tuple;
 import sirius.kernel.di.std.Register;
+import sirius.kernel.health.Log;
 import sirius.web.http.WebContext;
 import sirius.web.http.WebDispatcher;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Dispatches all URLs below <code>/assets</code>.
@@ -61,6 +67,14 @@ public class AssetsDispatcher implements WebDispatcher {
         if (url == null) {
             url = getClass().getResource("/assets/defaults" + uri.substring(7));
         }
+        if (url == null && uri.endsWith(".css")) {
+            String scssUri = uri.substring(0, uri.length() - 4) + ".scss";
+            url = getClass().getResource(scssUri);
+            if (url != null) {
+                handleSASS(ctx, uri, scssUri, url);
+                return true;
+            }
+        }
         if (url == null) {
             ctx.respondWith().error(HttpResponseStatus.NOT_FOUND);
         } else if ("file".equals(url.getProtocol())) {
@@ -69,5 +83,39 @@ public class AssetsDispatcher implements WebDispatcher {
             ctx.respondWith().resource(url.openConnection());
         }
         return true;
+    }
+
+    private static final Log SASS_LOG = Log.get("sass");
+
+    private static class SIRIUSGenerator extends Generator {
+        @Override
+        public void debug(String message) {
+            SASS_LOG.FINE(message);
+        }
+
+        @Override
+        public void warn(String message) {
+            SASS_LOG.WARN(message);
+        }
+    }
+
+    private void handleSASS(WebContext ctx, String cssUri, String scssUri, URL url) throws IOException {
+        URLConnection connection = url.openConnection();
+        String cacheKey = cssUri.substring(1).replaceAll("[^a-zA-Z0-9\\-_\\.]", "_");
+        File file = new File(new File("web-cache"), cacheKey);
+
+        if (!file.exists() || file.lastModified() < connection.getLastModified()) {
+            if (Sirius.isDev()) {
+                SASS_LOG.INFO("Compiling: " + scssUri);
+            }
+            SIRIUSGenerator gen = new SIRIUSGenerator();
+            gen.importStylesheet(scssUri);
+            gen.compile();
+            FileWriter writer = new FileWriter(file, false);
+            writer.write(gen.toString());
+            writer.close();
+        }
+
+        ctx.respondWith().named(cssUri.substring(cssUri.lastIndexOf("/") + 1)).file(file);
     }
 }
