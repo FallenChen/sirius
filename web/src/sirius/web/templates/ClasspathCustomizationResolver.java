3280d35dfc5e16a4ddb7bee63fdd3e89513eb2e3
aha <aha@scireum.de>
2014-08-28 17:12:57 +0200

    configurations have been renamed to customizations, as this term is less confusing

--- web/src/sirius/web/templates/ClasspathCustomizationResolver.java
+++ web/src/sirius/web/templates/ClasspathCustomizationResolver.java
@@ -14,19 +14,19 @@ import sirius.kernel.di.std.Register;
 import java.net.URL;
 
 /**
- * Simple resolver which tries to find the given resource in the classpath of the enabled configurations.
+ * Simple resolver which tries to find the given resource in the classpath of the enabled customizations.
  *
  * @author Andreas Haufler (aha@scireum.de)
  * @since 2014/08
  */
 @Register
-public class ClasspathConfigurationResolver implements Resolver {
+public class ClasspathCustomizationResolver implements Resolver {
 
     @Override
     public Resource resolve(String scopeId, String resource) {
         URL effectiveUrl = null;
         for (String config : Sirius.getActiveConfigurations()) {
-            URL url = getClass().getResource(resource.startsWith("/") ? "configurations/" + config + "/" + resource : "/configurations/" + config + "/" + resource);
+            URL url = getClass().getResource(resource.startsWith("/") ? "customizations/" + config + "/" + resource : "/customizations/" + config + "/" + resource);
             if (url != null) {
                 effectiveUrl = url;
             }
